49fd75732a368d6280c23bf7d067dfb515275143
Zak Richardson <zakrc.richardson@gmail.com>
2019-09-22 22:13:53 +0100

    Use .verify result for timestamp

--- lib/devise_two_factor/models/two_factor_authenticatable.rb
+++ lib/devise_two_factor/models/two_factor_authenticatable.rb
@@ -32,7 +32,8 @@ module Devise
         return false unless code.present? && otp_secret.present?
 
         totp = self.otp(otp_secret)
-        return consume_otp! if totp.verify(code, drift_ahead: self.class.otp_allowed_drift, drift_behind: self.class.otp_allowed_drift)
+        @consume_otp_time = totp.verify(code, drift_ahead: self.class.otp_allowed_drift, drift_behind: self.class.otp_allowed_drift)
+        return consume_otp! if @consume_otp_time
 
         false
       end
